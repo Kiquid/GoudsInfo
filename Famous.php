693fefeb04aae6fcc4847b3fb7127cc72dab2689
Kiquid <sameer@iconvix.com>
2018-08-29 16:28:49 +0530

    first commit with famous page

---
+++ Famous.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<meta name="author" content="">
+<meta name="description" content="">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Welcome to GoudInfo</title>
+
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon-1.png">
+<!-- Style Sheets -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="css/animate.css" type="text/css">
+<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
+<link rel="stylesheet" href="css/responsive_style.css" type="text/css">
+<!-- Font Awesome Fonts-->
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<!-- Google Fonts-->
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700,800%7CMontserrat:400,700' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+<div id="vfx_loader_block">
+  <div class="vfx-loader-item"> <img src="images/loading.gif" alt="" /> </div>
+</div>
+<div class="banner-image">
+   <img src="images/banner-1.jpg" height="100px" width="2500px" alt="Gouds Info banner image"/>
+ </div>
+ <!--
+ </div>-->
+<div id="logo-header" data-spy="affix" data-offset-top="500">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3 col-xs-9">
+        <!--<div id="logo">
+           <a href="index.html">
+          <img src="images/logo-goudInfo.png" alt="logo">
+        </a> </div>-->
+      </div>
+      <div class="col-sm-9">
+        <nav class="navbar navbar-default">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#thrift-1" aria-expanded="false"> <span class="sr-only">Toggle Navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          </div>
+          <div class="collapse navbar-collapse" id="thrift-1"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+            <div id="nav_menu_list">
+              <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="History.html">History</a></li>
+                <li><a href="categories.html">Categories</a></li>
+                <li><a href="Gallery.html">Gallery</a></li>
+                <li><a href="page_error_404.html">Post</a></li>
+                <!--
+                <li><a href="#">Listing <i class="fa fa-caret-down"></i></a>
+                  <ul class="dropdown">
+                    <li><a href="dashboard.html"><i class="fa fa-angle-double-right"></i> Dashboard</a></li>
+                    <li><a href="listing_grid.html"><i class="fa fa-angle-double-right"></i> Listing Grid</a></li>
+                    <li><a href="listing_list.html"><i class="fa fa-angle-double-right"></i> Listing List</a></li>
+                    <li><a href="listing_left_sidebar.html"><i class="fa fa-angle-double-right"></i> Listing Left Sidebar</a></li>
+                    <li><a href="listing_right_sidebar.html"><i class="fa fa-angle-double-right"></i> Listing Right Sidebar</a></li>
+                    <li><a href="listing_detail.html"><i class="fa fa-angle-double-right"></i> Listing Single Post</a></li>
+                    <li><a href="listing_fullwidth.html"><i class="fa fa-angle-double-right"></i> Listing Full Width</a></li>
+                    <li><a href="page_error_404.html"><i class="fa fa-angle-double-right"></i> Page not Found</a></li>
+                  </ul>
+                </li>
+                -->
+                <li><a href="contact.html">Contact Us</a></li>
+                <li class="btn_item">
+				    <ul>
+						<li>
+						  <button class="btn_login" data-toggle="modal" data-target="#login">Guest Sign</button>
+            </li>
+            <!--
+						<li>
+						  <button class="btn_register" data-toggle="modal" data-target="#register">Register</button>
+            </li>
+            -->
+				    </ul>	
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="breadcrum-inner-block">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="breadcrum-inner-header">
+          <h1>Famous Gouds</h1>
+          <a href="index.html">Home</a> <i class="fa fa-circle"></i> <a href="History.html">
+            <span style="color: #ff4300">Famous Gouds</span></a> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="about-company">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12 v-center">
+        <div class="about-heading-title bt_heading_3">
+          <h1 style="color: #ff4300; margin-left: 1px;">Famous Goud </h1>         
+          <div class="blind line_1" style="width:75px;"></div>          
+          <div class="flipInX-1 blind icon"><span class="icon"><i class="fa fa-stop">
+
+          </i>&nbsp;&nbsp;<i class="fa fa-stop"></i></span></div>
+          <div class="blind line_2" style="width:95px;"></div>
+        </div>
+      
+              
+      </div>
+      <!--
+      <div class="col-md-4 col-sm-12 col-xs-12 user-lt-above"> <img src="images/about-user.png" alt="about-user"> </div>-->
+    </div>
+  </div>
+ 
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/kumar_Goud.png" alt="" width="150px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>V. Kumar Goud</B></P> 
+        <P style="padding-right: 15px;"><B>Founder President</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/tulla-1.png" alt="" width="150px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Tulla Veerender Goud</B></P> 
+        <P style="padding-right: 15px;"><B>Patron</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/vijay-2.png" alt="" width="150px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Dr.Vijay Bhaskar B</B></P> 
+        <P style="padding-right: 15px;"><B>Vice President - Health</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/valab.png" alt="" width="158px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>A.Valab Das Goud</B></P> 
+        <P style="padding-right: 15px;"><B>Director Infrastructure</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/hemanth-1.png" alt="" width="150px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Hemanth kumar Thokala</B></P> 
+        <P style="padding-right: 15px;"><B>Secretary General</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/vivekananda.png" alt="" width="173px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Vivekananda Goud Tigulla</B></P> 
+        <P style="padding-right: 15px;"><B>Treasure</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/Anil.png" alt="" width="140px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Anil Rachamalla</B></P> 
+        <P style="padding-right: 15px;"><B>Director-Operations</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/bala.png" alt="" width="150px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Dr.Gopagoni Anand Goud </B></P> 
+        <P style="padding-right: 15px;"><B>Director Student & youth Development</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/rahul.png" alt="" width="180px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Rahul Goud K</B></P> 
+        <P style="padding-right: 15px;"><B>Director HR Development</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/indira.png" alt="" width="180px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Ms.Indira Shoban</B></P> 
+        <P style="padding-right: 15px;"><B>Director Women Welfare</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/karitha.png" alt="" width="170px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Ms.karthika</B></P> 
+        <P style="padding-right: 15px;"><B>Director Women Empowerment</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/bhaskar.png" alt="" width="155px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>M.Bhaskar Goud</B></P> 
+        <P style="padding-right: 15px;"><B>Director Community Service</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/srinivas.png" alt="" width="150px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>M.Srinivas Goud</B></P> 
+        <P style="padding-right: 15px;"><B>Director Information Technology</B></P>             
+      </div>           
+      <div class="col-md-3 col-sm-4 col-xs-4">       
+        <img src="images/Raghavendra.png" alt="" width="140px;" style="padding: 25px 0px 25px;">  <br>
+        <P style="padding-right: 15px;"><B>Raghavendra Goud</B></P> 
+        <P style="padding-right: 15px;"><B>Director Membership Development</B></P>             
+      </div>           
+    </div>   
+</div>
+<!-- Footer Section-->
+<footer class="site-footer footer-map">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <h2>About Us</h2>
+          <hr>
+          <p class="about-lt">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer justo lectus,
+             consectetur quis nisi vitae, Nunc eget ultrices ligula.</p>
+          <a href="about.html" class="btn-primary-link more-detail"><i class="fa fa-hand-o-right"></i> Read More</a>
+          <h2>Follow Us</h2>
+          <hr>
+          <ul class="social-icons">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+           <!-- <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+            <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
+           -->
+          </ul>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <h2>Useful links</h2>
+          <hr>
+          <ul class="use-slt-link">
+            <li><a href="#"><i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Privacy & Policy</a></li>
+           <!-- <li><a href="#"><i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Payment Method</a></li>-->
+            <li><a href="#"><i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Sitemap</a></li>
+            <li><a href="#"><i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Support</a></li>
+            <li><a href="#"><i class="fa fa-hand-o-right"></i>&nbsp;&nbsp;Terms & Condition</a></li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12">
+          <p class="text-xs-center">Copyright © 2018 All Rights Reserved.</p>
+          <p class="text-xs-center"><a href="http://www.iconvix.com/"  target="_blank">Designed by Iconvix Business Solutions</a> </p>
+        </div>
+        <div><a href="#" class="scrollup">Scroll</a></div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!--================================ Login and Register Forms ===========================================--> 
+
+<!-- login form -->
+<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="listing-modal-1 modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel"> Login</h4>
+      </div>
+      <div class="modal-body">
+        <div class="listing-login-form">
+          <form action="#">
+            <div class="listing-form-field"> <i class="fa fa-user blue-1"></i>
+              <input class="form-field bgwhite" type="text" name="user_name" placeholder="Username" />
+            </div>
+            <div class="listing-form-field"> <i class="fa fa-lock blue-1"></i>
+              <input class="form-field bgwhite" type="password" name="user_pass" placeholder="Password"  />
+            </div>
+            <div class="listing-form-field clearfix margin-top-20 margin-bottom-20">
+              <input type="checkbox" id="checkbox-1-1" class="regular-checkbox" />
+              <label for="checkbox-1-1"></label>
+              <label class="checkbox-lable">Remember Me</label>
+              <a href="#">Forgot Password?</a> </div>
+            <div class="listing-form-field">
+              <input class="submit" type="submit" value="login" />
+            </div>
+          </form>
+          <div class="bottom-links">
+            <p>Not a Member?<a href="#">Create Account</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- registration form -->
+<div class="modal fade" id="register" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="listing-modal-1 modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="myModalLabel2">Registration</h4>
+      </div>
+      <div class="modal-body">
+        <div class="listing-register-form">
+          <form action="#">
+            <div class="listing-form-field"> <i class="fa fa-user blue-1"></i>
+              <input class="form-field bgwhite" type="text" name="user_name" placeholder="Name"  />
+            </div>
+            <div class="listing-form-field"> <i class="fa fa-envelope blue-1"></i>
+              <input class="form-field bgwhite" type="email" name="user_email" placeholder="Email" />
+            </div>
+            <div class="listing-form-field"> <i class="fa fa-lock blue-1"></i>
+              <input class="form-field bgwhite" type="password" name="user_password" placeholder="Password"  />
+            </div>
+            <div class="listing-form-field"> <i class="fa fa-lock blue-1"></i>
+              <input class="form-field bgwhite" type="password" name="user_confirm_password" placeholder="Confirm Password" />
+            </div>
+            <div class="listing-form-field clearfix margin-top-20 margin-bottom-20 login_form_text_center">
+              <input type="checkbox" id="checkbox-1-2" class="regular-checkbox" />
+              <label for="checkbox-1-2"></label>
+              <label class="checkbox-lable">i agree with</label> &nbsp; <a href="#">Terms & Conditions</a> </div>
+            <div class="listing-form-field">
+              <input class="submit" type="submit" value="create account" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Scripts --> 
+<script src="js/jquery-2.2.4.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/waypoints.js"></script> 
+<script src="js/jquery_counterup.js"></script> 
+<script src="js/jquery_custom.js"></script> 
+</body>
+</html>
